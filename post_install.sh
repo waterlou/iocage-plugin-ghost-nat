d0d07064fadc5cd415bc660d76dd2368446df4fa
Water Lou <water@waterworld.com.hk>
2020-04-25 13:31:19 +0800

    run as www

--- post_install.sh
+++ post_install.sh
@@ -12,10 +12,14 @@ cd ${GHOST_PATH}
 # create config file
 ghost install --db sqlite3 --no-prompt --no-stack --no-setup
 ghost config --ip 0.0.0.0 --port 2368 --no-prompt --db sqlite3 --url http://localhost:2368
+ghost config set process local
 
 # install sqlite3 manually
 npm install sqlite3 --sqlite=/usr/local
 find ./ ! -path "./versions/*" -type f -exec chmod 664 {} \;
+
+# change owner
+chown -R www:www ${GHOST_PATH}
 
 # Configure
 sysrc ghost_enable=yes
