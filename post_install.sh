f2e065b368c01e33fb3a2df8a3e413f5ed318fb4
Water Lou <water@waterworld.com.hk>
2020-07-29 10:01:37 +0800

    update release to 12.1

--- post_install.sh
+++ post_install.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
 GHOST_PATH=/usr/local/share/ghost
+LOG_PATH=/var/log/ghost
 
 # Install node packages
 npm -g install ghost-cli
 
-# Use ghost cli to install source
+mkdir -p ${LOG_PATH}
+chown www:www ${LOG_PATH}
+
 mkdir -p ${GHOST_PATH}
 cd ${GHOST_PATH}
 
-# create config file
+# install and create config file
 ghost install --db sqlite3 --no-prompt --no-stack --no-setup
 ghost config --ip 0.0.0.0 --port 2368 --no-prompt --db sqlite3 --url http://localhost:2368
 ghost config set process local
+ghost config set logging.path /var/log/ghost/
 
 # install sqlite3 manually
 npm install sqlite3 --sqlite=/usr/local
@@ -23,6 +27,7 @@ chown -R www:www ${GHOST_PATH}
 
 # Configure
 sysrc ghost_enable=yes
+sysrc ghost_url=http://localhost:2368
 
 # Start service
 service ghost start
